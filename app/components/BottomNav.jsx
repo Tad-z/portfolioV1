d751cd7cb336a1582df64cb8cfe7d5cc8168578f
Tad-z <david.akintade1000@gmail.com>
2024-02-10 14:06:23 +0100

    push

--- app/components/BottomNav.jsx
+++ app/components/BottomNav.jsx
@@ -6,7 +6,7 @@ import { HiMail } from 'react-icons/hi'
 
 export default function BottomNav({darkMode}) {
   const mobileNavStyles = {
-    background: darkMode? "#01182ede" : "#ffffff96",
+    background: darkMode? "#252529" : "#ffffff96",
     color: darkMode? "#e1e1e1" : "#1b2b26",
     border: darkMode? "1px solid transparent" : "1px solid #a1a1a1"
   }
